a7f799b1143847cbc45a21e25dd7d93fad79d021
diegopso <diegopso2@gmail.com>
2014-04-30 16:32:25 -0300

    first commit

---
+++ src/Require/Module.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Dor\Require;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        (new ModuleRouteListener())->attach($e->getApplication()->getEventManager());
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '../../config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__,
+                ),
+            ),
+        );
+    }
+}
